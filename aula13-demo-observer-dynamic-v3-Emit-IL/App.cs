2c64f24172db583034c4c5c5a0acec55fa68f581
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-04-05 12:24:04 +0100

    aula 13 - setup

---
+++ aula13-demo-observer-dynamic-v3-Emit-IL/App.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Windows.Forms;
+
+
+class App {
+    static void Main() {
+        Counter pub = new Counter(3);
+        
+        /*
+         * Adiciona dinamicamente todos os métodos compatíveis com 
+         * a descritor de Update da interface Subscriber.
+         */
+        pub.AddSubscriberDynamic(new MySubscribers());
+        
+        pub.Start();
+    }
+}
+
+public class MySubscribers {
+    
+    public static void Dummy(int nr, long a) 
+    {
+    }
+
+    public int ConsoleAlert(int nr) 
+    {
+        Console.WriteLine("Count = {0}", nr);
+        return nr++;
+    }
+
+    public static void  MboxAlert(int nr) 
+    {
+        string msg = String.Format("Count = {0}", nr);
+        MessageBox.Show(msg);
+    }
+}
+
